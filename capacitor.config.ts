b62a181d46845e517e4f1422d1ed4fa348e083b0
regtat <regitaghani@gmail.com>
2024-10-29 21:59:31 +0700

    Tugas 6 Pertemuan 7

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LabMobile6_RegitaMaulia_C',
+  webDir: 'www'
+};
+
+export default config;
